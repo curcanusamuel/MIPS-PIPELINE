e8bd519f2a4280f586528c14aae482392677de1b
curcanusamuel <126792709+curcanusamuel@users.noreply.github.com>
2024-02-12 18:43:22 +0200

    Add files via upload

---
+++ PROIECT PG/ProiectFinal/main.cpp
@@ -0,0 +1,533 @@
+﻿#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp> //core glm functionality
+#include <glm/gtc/matrix_transform.hpp> //glm extension for generating common transformation matrices
+#include <glm/gtc/matrix_inverse.hpp> //glm extension for computing inverse matrices
+#include <glm/gtc/type_ptr.hpp> //glm extension for accessing the internal data structure of glm types
+#include <iostream>
+#include "SkyBox.hpp"
+
+#include "Window.h"
+#include "Shader.hpp"
+#include "Camera.hpp"
+#include "Model3D.hpp"
+
+#include <iostream>
+
+// window
+gps::Window myWindow;
+
+// matrices
+glm::mat4 model;
+glm::mat4 view;
+glm::mat4 projection;
+glm::mat3 normalMatrix;
+
+// light parameters
+glm::vec3 lightDir;
+glm::vec3 lightColor;
+
+
+int retina_height;
+int retina_width;
+// shader uniform locations
+GLint modelLoc;
+GLint viewLoc;
+GLint projectionLoc;
+GLint normalMatrixLoc;
+GLint lightDirLoc;
+GLint lightColorLoc;
+
+// camera
+gps::Camera myCamera(
+    glm::vec3(0.0f, 2.0f, 6.0f),
+    glm::vec3(0.0f, 0.0f, 0.0f));
+glm::mat4 lightRotation;
+GLfloat cameraSpeed = 0.4f;
+GLboolean pressedKeys[1024];
+
+// models
+gps::Model3D lightCube,screenQuad;
+gps::Model3D mapa;
+gps::Model3D dragon;
+GLfloat angleY=0;
+// shaders
+const unsigned int SHADOW_WIDTH = 5000;
+const unsigned int SHADOW_HEIGHT = 5000;
+gps::Shader depthMapShader;
+gps::Shader myCustomShader;
+gps::Shader lightShader;
+gps::Shader screenQuadShader;
+gps::SkyBox mySkyBox;
+gps::Shader skyboxShader;
+GLfloat lightAngle=0;
+GLuint shadowMapFBO;
+GLuint depthMapTexture;
+bool firstMouse = true;
+float movementSpeed = 2; // units per second
+float rotationM = 0;
+double lastTimeStamp = glfwGetTime();
+void updateRoatation(double elapsedSeconds) {
+    rotationM = rotationM + movementSpeed * elapsedSeconds;
+}
+
+bool showDepthMap;
+
+GLenum glCheckError_(const char* file, int line)
+{
+    GLenum errorCode;
+    while ((errorCode = glGetError()) != GL_NO_ERROR) {
+        std::string error;
+        switch (errorCode) {
+        case GL_INVALID_ENUM:
+            error = "INVALID_ENUM";
+            break;
+        case GL_INVALID_VALUE:
+            error = "INVALID_VALUE";
+            break;
+        case GL_INVALID_OPERATION:
+            error = "INVALID_OPERATION";
+            break;
+        case GL_STACK_OVERFLOW:
+            error = "STACK_OVERFLOW";
+            break;
+        case GL_STACK_UNDERFLOW:
+            error = "STACK_UNDERFLOW";
+            break;
+        case GL_OUT_OF_MEMORY:
+            error = "OUT_OF_MEMORY";
+            break;
+        case GL_INVALID_FRAMEBUFFER_OPERATION:
+            error = "INVALID_FRAMEBUFFER_OPERATION";
+            break;
+        }
+        std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+    }
+    return errorCode;
+}
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
+
+void windowResizeCallback(GLFWwindow* window, int width, int height)
+{
+    fprintf(stdout, "window resized to width: %d , and height: %d\n", width, height);
+
+    glfwGetFramebufferSize(myWindow.getWindow(), &retina_width, &retina_height);
+
+    myCustomShader.useShaderProgram();
+
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)retina_width / (float)retina_height, 0.1f, 1000.0f);
+    //send matrix data to shader
+    GLint projLoc = glGetUniformLocation(myCustomShader.shaderProgram, "projection");
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+    lightShader.useShaderProgram();
+
+    glUniformMatrix4fv(glGetUniformLocation(lightShader.shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+    depthMapShader.useShaderProgram();
+
+    //set Viewport transform
+    glViewport(0, 0, retina_width, retina_height);
+
+    glfwSetInputMode(myWindow.getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+}
+
+void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+    if (key == GLFW_KEY_M && action == GLFW_PRESS)
+        showDepthMap = !showDepthMap;
+
+    if (key >= 0 && key < 1024) {
+        if (action == GLFW_PRESS) {
+            pressedKeys[key] = true;
+        }
+        else if (action == GLFW_RELEASE) {
+            pressedKeys[key] = false;
+        }
+    }
+}
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
+    myCamera.mouseCallback(window, xpos, ypos);
+}
+
+void processMovement() {
+    if (pressedKeys[GLFW_KEY_W]) {
+       
+        
+        if (mapa.checkCollision(myCamera.getCameraPosition()))
+                return;
+        myCamera.move(gps::MOVE_FORWARD, cameraSpeed);
+
+        view = myCamera.getViewMatrix();
+        myCustomShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+
+    if (pressedKeys[GLFW_KEY_S]) {
+        
+        myCamera.move(gps::MOVE_BACKWARD, cameraSpeed);
+        //update view matrix
+        view = myCamera.getViewMatrix();
+        myCustomShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+
+    if (pressedKeys[GLFW_KEY_A]) {
+      
+        myCamera.move(gps::MOVE_LEFT, cameraSpeed);
+        //update view matrix
+        view = myCamera.getViewMatrix();
+        myCustomShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+
+    if (pressedKeys[GLFW_KEY_D]) {
+       
+
+        myCamera.move(gps::MOVE_RIGHT, cameraSpeed);
+        //update view matrix
+        view = myCamera.getViewMatrix();
+        myCustomShader.useShaderProgram();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        // compute normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+
+    if (pressedKeys[GLFW_KEY_Q]) {
+        angleY -= 1.0f;
+        // update model matrix for teapot
+        model = glm::rotate(glm::mat4(1.0f), glm::radians(angleY), glm::vec3(0, 1, 0));
+        // update normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+
+    if (pressedKeys[GLFW_KEY_E]) {
+        angleY += 1.0f;
+        // update model matrix for teapot
+        model = glm::rotate(glm::mat4(1.0f), glm::radians(angleY), glm::vec3(0, 1, 0));
+        // update normal matrix for teapot
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    }
+    if (pressedKeys[GLFW_KEY_J]) {
+        myCustomShader.useShaderProgram();
+
+
+        lightAngle -= 1.0f;
+        model = glm::rotate(glm::mat4(1.0f), glm::radians(angleY), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+        glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+    }
+
+    if (pressedKeys[GLFW_KEY_L]) {
+        myCustomShader.useShaderProgram();
+
+        lightAngle += 1.0f;
+        model = glm::rotate(glm::mat4(1.0f), glm::radians(angleY), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+        glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+    }
+    if (pressedKeys[GLFW_KEY_Z]) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    }
+    if (pressedKeys[GLFW_KEY_X]) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+    if (pressedKeys[GLFW_KEY_C]) {
+        glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
+    }
+}
+
+void initOpenGLWindow() {
+    myWindow.Create(1024, 768, "OpenGL Project Core");
+    glfwGetFramebufferSize(myWindow.getWindow(), &retina_width, &retina_height);
+}
+
+void setWindowCallbacks() {
+    glfwSetWindowSizeCallback(myWindow.getWindow(), windowResizeCallback);
+    glfwSetKeyCallback(myWindow.getWindow(), keyboardCallback);
+    glfwSetCursorPosCallback(myWindow.getWindow(), mouseCallback);
+}
+
+void initOpenGLState() {
+    glClearColor(0.7f, 0.7f, 0.7f, 1.0f);
+    glViewport(0, 0, myWindow.getWindowDimensions().width, myWindow.getWindowDimensions().height);
+    glEnable(GL_FRAMEBUFFER_SRGB);
+    glEnable(GL_DEPTH_TEST); // enable depth-testing
+    glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+    glEnable(GL_CULL_FACE); // cull face
+    glCullFace(GL_BACK); // cull back face
+    glFrontFace(GL_CCW); // GL_CCW for counter clock-wise
+}
+
+void initModels() {
+    screenQuad.LoadModel("objects/quad/quad.obj");
+    mapa.LoadModel("objects/mapa/mapa.obj");
+    dragon.LoadModel("objects/mapa/dragon.obj");
+
+}
+
+void initShaders() {
+    myCustomShader.loadShader("shaders/basic.vert", "shaders/basic.frag");
+    myCustomShader.useShaderProgram();
+    lightShader.loadShader("shaders/lightCube.vert", "shaders/lightCube.frag");
+    lightShader.useShaderProgram();
+    screenQuadShader.loadShader("shaders/screenQuad.vert", "shaders/screenQuad.frag");
+    screenQuadShader.useShaderProgram();
+    depthMapShader.loadShader("shaders/nanosuitShadow.vert", "shaders/nanosuitShadow.frag");
+    depthMapShader.useShaderProgram();
+}
+glm::mat4 computeLightSpaceTrMatrix() {
+    glm::mat4 lightView = glm::lookAt(glm::mat3(lightRotation) * lightDir*35.0f, glm::vec3(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+    const GLfloat near_plane = 0.1f, far_plane = 110.0f;
+    glm::mat4 lightProjection = glm::ortho(-110.0f, 110.0f, -110.0f, 110.0f, near_plane, far_plane);
+    glm::mat4 lightSpaceMatrix = lightProjection * lightView;
+    return lightSpaceMatrix;
+}
+void initFBO() {
+    glGenFramebuffers(1, &shadowMapFBO);
+    glGenTextures(1, &depthMapTexture);
+    glBindTexture(GL_TEXTURE_2D, depthMapTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT,
+        SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    glBindFramebuffer(GL_FRAMEBUFFER, shadowMapFBO);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthMapTexture, 0);
+
+    glDrawBuffer(GL_NONE);
+    glReadBuffer(GL_NONE);
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+}
+void initUniforms() {
+    myCustomShader.useShaderProgram();
+
+    // create model matrix for teapot
+    model = glm::rotate(glm::mat4(1.0f), glm::radians(angleY), glm::vec3(0.0f, 1.0f, 0.0f));
+    modelLoc = glGetUniformLocation(myCustomShader.shaderProgram, "model");
+
+    // get view matrix for current camera
+    view = myCamera.getViewMatrix();
+    viewLoc = glGetUniformLocation(myCustomShader.shaderProgram, "view");
+    // send view matrix to shader
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+    // compute normal matrix for teapot
+    normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    normalMatrixLoc = glGetUniformLocation(myCustomShader.shaderProgram, "normalMatrix");
+
+    // create projection matrix
+    projection = glm::perspective(glm::radians(45.0f),
+        (float)myWindow.getWindowDimensions().width / (float)myWindow.getWindowDimensions().height,
+        0.1f, 1000.0f);
+    projectionLoc = glGetUniformLocation(myCustomShader.shaderProgram, "projection");
+    // send projection matrix to shader
+    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+    //set the light direction (direction towards the light)
+    lightDir = glm::vec3(0.0f,1.0f, 1.0f);
+    lightDirLoc = glGetUniformLocation(myCustomShader.shaderProgram, "lightDir");
+    // send light dir to shader
+    glUniform3fv(lightDirLoc, 1, glm::value_ptr(lightDir));
+
+    //set light color
+    lightColor = glm::vec3(0.8f, 0.8f, 0.8f); //white light
+    lightColorLoc = glGetUniformLocation(myCustomShader.shaderProgram, "lightColor");
+    // send light color to shader
+    glUniform3fv(lightColorLoc, 1, glm::value_ptr(lightColor));
+}
+
+void drawDragonRotate(float x, float y, float z, gps::Shader shader, bool depthPass) {
+    model = glm::mat4(1.0f);
+
+    // Rotirea lentă în jurul axei X
+    model = glm::rotate(model, rotationM * -0.01f, glm::vec3(0, 0, 1));
+
+    // Translația la poziția specificată
+    model = glm::translate(model, glm::vec3(x, y, z));
+
+    // Scara inițială a dragonului
+    model = glm::scale(model, glm::vec3(1));
+
+    // Rotirea lentă în jurul axei Y
+    model = glm::rotate(model, rotationM * -0.1f, glm::vec3(0, 0, 1));
+
+    // Adăugarea unei scalări suplimentare pentru a micșora dimensiunile dragonului
+    model = glm::scale(model, glm::vec3(1));
+
+    glUniformMatrix4fv(glGetUniformLocation(shader.shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+    if (!depthPass) {
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+        glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+    }
+}
+
+void dragonDraw(gps::Shader shader, bool depthPass) {
+    drawDragonRotate(0, 0, 0, shader, depthPass);
+    dragon.Draw(shader);
+}
+void drawObjects(gps::Shader shader, bool depthPass) {
+    
+
+    shader.useShaderProgram();
+    dragonDraw(shader, depthPass);
+
+    model = glm::mat4(1.0f);
+    glUniformMatrix4fv(glGetUniformLocation(shader.shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+  
+    if (!depthPass) {
+        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+        glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+    }
+
+    mapa.Draw(shader);
+   
+}
+
+void initSkyBox() {
+    std::vector<const GLchar*> faces;
+    faces.push_back("skybox/right.tga");
+    faces.push_back("skybox/left.tga");
+    faces.push_back("skybox/top.tga");
+    faces.push_back("skybox/bottom.tga");
+    faces.push_back("skybox/back.tga");
+    faces.push_back("skybox/front.tga");
+    mySkyBox.Load(faces);
+    skyboxShader.loadShader("shaders/skyboxShader.vert", "shaders/skyboxShader.frag");
+    skyboxShader.useShaderProgram();
+}
+void renderScene() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    // select active shader program
+    double currentTimeStamp = glfwGetTime();
+    updateRoatation(currentTimeStamp - lastTimeStamp);
+    lastTimeStamp = currentTimeStamp;
+    depthMapShader.useShaderProgram();
+    glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT);
+    glBindFramebuffer(GL_FRAMEBUFFER, shadowMapFBO);
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glUniformMatrix4fv(glGetUniformLocation(depthMapShader.shaderProgram, "lightSpaceTrMatrix"),
+        1,
+        GL_FALSE,
+        glm::value_ptr(computeLightSpaceTrMatrix()));
+    drawObjects(depthMapShader, true);
+    mySkyBox.Draw(skyboxShader, view, projection);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // render depth map on screen - toggled with the M key
+    
+    if (showDepthMap) {
+        glViewport(0, 0, retina_width, retina_height);
+        glClear(GL_COLOR_BUFFER_BIT);
+        screenQuadShader.useShaderProgram();
+
+        //bind the depth map
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, depthMapTexture);
+        glUniform1i(glGetUniformLocation(screenQuadShader.shaderProgram, "depthMap"), 0);
+
+
+        glDisable(GL_DEPTH_TEST);
+        screenQuad.Draw(screenQuadShader);
+        glEnable(GL_DEPTH_TEST);
+    }
+    else {
+
+        // final scene rendering pass (with shadows)
+
+        glViewport(0, 0, retina_width, retina_height);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        myCustomShader.useShaderProgram();
+
+        view = myCamera.getViewMatrix();
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+        lightRotation = glm::rotate(glm::mat4(1.0f), glm::radians(lightAngle), glm::vec3(0.0f, 1.0f, 0.0f));
+        glUniform3fv(lightDirLoc, 1, glm::value_ptr(glm::inverseTranspose(glm::mat3(view * lightRotation)) * lightDir));
+
+        //bind the shadow map
+        glActiveTexture(GL_TEXTURE3);
+        glBindTexture(GL_TEXTURE_2D, depthMapTexture);
+        glUniform1i(glGetUniformLocation(myCustomShader.shaderProgram, "shadowMap"), 3);
+
+        glUniformMatrix4fv(glGetUniformLocation(myCustomShader.shaderProgram, "lightSpaceTrMatrix"),
+            1,
+            GL_FALSE,
+            glm::value_ptr(computeLightSpaceTrMatrix()));
+
+        drawObjects(myCustomShader, false);
+        mySkyBox.Draw(skyboxShader, view, projection);
+        //draw a white cube around the light
+
+        lightShader.useShaderProgram();
+        
+      
+    }
+
+}
+
+void cleanup() {
+    myWindow.Delete();
+    glDeleteTextures(1, &depthMapTexture);
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    glDeleteFramebuffers(1, &shadowMapFBO);
+    glfwTerminate();
+}
+
+int main(int argc, const char* argv[]) {
+
+    try {
+        initOpenGLWindow();
+    }
+    catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    initOpenGLState();
+    initModels();
+    initShaders();
+    initUniforms();
+    initFBO();
+    setWindowCallbacks();
+    initSkyBox();
+    glCheckError();
+    while (!glfwWindowShouldClose(myWindow.getWindow())) {
+        processMovement();
+        renderScene();
+
+        glfwPollEvents();
+        glfwSwapBuffers(myWindow.getWindow());
+
+        glCheckError();
+    }
+
+    cleanup();
+
+    return EXIT_SUCCESS;
+}
+
+
+
+
